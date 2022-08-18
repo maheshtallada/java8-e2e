b647dbac1c4255e446861e4938f91f430a6175ba
Mahesh Gupta Tallada <mahesh.tallada@hotmail.com>
2022-08-18 22:23:53 +0530

    Sort objects using traditional and lambda, stream api

--- src/main/java/com/learning/java8/java8e2e/Java8E2eApplication.java
+++ src/main/java/com/learning/java8/java8e2e/Java8E2eApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import sorting.CollectionSort;
 import sorting.Employee;
+import sorting.SortObjectsTrdtnlAndLambda;
 import sorting.SortPrimitiveTrdtnlAndLambda;
 
 @SpringBootApplication
@@ -26,7 +27,9 @@ public class Java8E2eApplication {
 		//CollectionSort.sortByIdAndThenSalary(Arrays.asList(employees));
 
 		//SortPrimitiveTrdtnlAndLambda.sortTraditional();
-		SortPrimitiveTrdtnlAndLambda.sortLambda();
+		//SortPrimitiveTrdtnlAndLambda.sortLambda();
+		
+		SortObjectsTrdtnlAndLambda.sortStream();
 		
 	}
 
---
+++ src/main/java/sorting/SortObjectsTrdtnlAndLambda.java
@@ -0,0 +1,52 @@
+package sorting;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortObjectsTrdtnlAndLambda {
+	// we will see how to sort primitives in traditional and lambda
+	
+	public static void sortTraditional() {
+		List<EmployeeV2> employeeV2s = Database.getEmployeeV2s();
+		
+		//sort based on salary -- asc/desc based on over ridden compare method
+		Collections.sort(employeeV2s, new MyComparator());
+		System.out.println(employeeV2s);
+		
+	}
+	
+	public static void sortStream() {
+		List<EmployeeV2> employeeV2s = Database.getEmployeeV2s();
+		
+		//using lambda
+		Collections.sort(employeeV2s, (o1,  o2)-> (int) (o2.getSalary() - o1.getSalary()));
+		
+		System.out.println("Using Lambda");
+		System.out.println(employeeV2s);
+		
+		
+		// using stream api
+		System.out.println("Using stream");
+		employeeV2s
+			.stream()
+			.sorted((o1,  o2)-> (int) (o2.getSalary() - o1.getSalary()))
+			.forEach(System.out::println);
+		
+	}
+
+}
+
+
+class MyComparator implements Comparator<EmployeeV2> {
+
+	@Override
+	public int compare(EmployeeV2 o1, EmployeeV2 o2) {
+		//asc
+		//return (int) (o1.getSalary() - o2.getSalary());
+		
+		//desc
+		return (int) (o2.getSalary() - o1.getSalary());
+	}
+	
+}
