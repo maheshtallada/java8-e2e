7daa32f78535085dea52d6439137950d87f6a4e8
Mahesh Tallada <mahesh.tallada@gmail.com>
2022-09-01 15:56:43 +0530

    Creational DP -- Singleton

---
+++ src/main/java/com/learning/java8/java8e2e/designPatterns/creational/EagerA.java
@@ -0,0 +1,23 @@
+package com.learning.java8.java8e2e.designPatterns.creational;
+
+public class EagerA {
+	
+	/*
+	 * Create a class with static member which is the object, assign it with a new instance ();
+	 * add private no args constructor
+	 * add static method, which returns the above static variable.
+	 * */
+	
+	private static EagerA eagerA = new EagerA();
+	
+	private EagerA () {}
+	
+	public static EagerA getEagerA( ) {
+		return eagerA;
+	}
+	
+	public void showMessage() {
+		System.out.println("This is a singleton class demo called through eager Instantiation");
+	}
+
+}
---
+++ src/main/java/com/learning/java8/java8e2e/designPatterns/creational/LazyA.java
@@ -0,0 +1,31 @@
+package com.learning.java8.java8e2e.designPatterns.creational;
+
+public class LazyA {
+	
+	/*
+	 * Create a class with static member which is the LazyA. 
+	 * Just declare, don't assign an instance with new operator.
+	 * add private no args constructor
+	 * add static method, as below
+	 * */
+	
+	private static LazyA lazyA;
+	
+	private LazyA () {}
+	
+	public static LazyA getLazyA() {
+		if (lazyA == null) {
+			synchronized (LazyA.class) {
+				if (lazyA == null) {
+					lazyA = new LazyA();// instance will be created at request time
+				}
+			}
+		}
+		return lazyA;
+	}
+	
+	public void showMessage() {
+		System.out.println("This is a singleton class demo called through lazy Instantiation");
+	}
+
+}
