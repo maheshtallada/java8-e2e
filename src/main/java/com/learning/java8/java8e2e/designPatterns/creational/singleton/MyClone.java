0951fe0f49209b5750d420b68d94075d315f2ba2
Mahesh Gupta Tallada <mahesh.tallada@hotmail.com>
2022-09-17 12:30:51 +0530

    Various ways of breaking singleton class and code to avoid breaking & make it pure Singleton

---
+++ src/main/java/com/learning/java8/java8e2e/designPatterns/creational/singleton/MyClone.java
@@ -0,0 +1,9 @@
+package com.learning.java8.java8e2e.designPatterns.creational.singleton;
+
+public class MyClone implements Cloneable {
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+}
--- src/main/java/com/learning/java8/java8e2e/designPatterns/creational/singleton/Singleton.java
+++ src/main/java/com/learning/java8/java8e2e/designPatterns/creational/singleton/Singleton.java
@@ -1,5 +1,16 @@
 package com.learning.java8.java8e2e.designPatterns.creational.singleton;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Singleton {
 	
 	/*There are two forms of singleton design pattern
@@ -16,7 +27,22 @@ public class Singleton {
 	// java.lang.runtime is the best example for singleton class 
 	// --since runtime is unique, there should be only single implementation of the class
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CloneNotSupportedException, 
+		InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, 
+		FileNotFoundException, IOException, ClassNotFoundException {
+		/*
+		 * CloneNotSupportedException is added while trying to clone the object using clone method
+		 * 
+		 * InstantiationException, IllegalAccessException, IllegalArgumentException and InvocationTargetException
+		 * are added while trying to create instance using reflection API 
+		 * 
+		 * FileNotFoundException and IOException, ClassNotFoundException
+		 * are added while trying to deserialize the initial instance.
+		 */
+		
+		
+		
+		
 		
 		// Early
 		EagerA eagerA = EagerA.getEagerA();
@@ -27,6 +53,58 @@ public class Singleton {
 		
 		// Lazy inner class
 		LazyInnerClass.getInstance().showMessage();
+		
+		
+		
+		PreventReflection pr = PreventReflection.getInstance();
+		System.out.println(pr.hashCode());
+		
+		// break singleton using clone
+		
+		// *************************************(UN)COMMENT THIS FOR CLONING OBJECT **************************//
+		/*PreventReflection pr2 = (PreventReflection) pr.clone();
+		System.out.println(pr2.hashCode());
+		// prevented this break by overriding clone method and returning CloneNotSupportedException.
+		*/
+		
+		
+		
+		
+		
+		// break using Reflection -- for time being comment out "break singleton using clone"
+		/* Reflection API - Java Reflection is a process of examining or modifying 
+		   the run time behavior of a class at run time */
+		
+		// *******************(UN)COMMENT THIS FOR CREATING OBJECT USING REFLECTION**************************//
+		/*PreventReflection reflectionInstance = null;
+		
+		Constructor<?>[] declaredConstructors = PreventReflection.class.getDeclaredConstructors();
+		for (Constructor<?> constructor : declaredConstructors) {
+			constructor.setAccessible(true);
+			reflectionInstance = (PreventReflection) constructor.newInstance();
+		}
+		System.out.println(reflectionInstance.hashCode());
+		// prevented this by adding a condition in private constructor, if instance is created, throw exception. */
+		
+		
+		
+		
+		
+		
+		// break using de-serialization. prerequisite: class must implement Serializable
+		
+		// *******************(UN)COMMENT THIS FOR CREATING OBJECT USING DE-SERIALIZATION**************************//
+		/* // Serialize singleton object to a file.
+		ObjectOutput out = new ObjectOutputStream(new FileOutputStream("singleton.ser"));
+		out.writeObject(pr);
+		out.close();
+
+		// Deserialize singleton object from the file
+		ObjectInput in = new ObjectInputStream(new FileInputStream("singleton.ser"));
+		PreventReflection instance2 = (PreventReflection) in.readObject();
+		in.close();
+
+		System.out.println("instance2 hashCode: " + instance2.hashCode()); */
 
 	}
 
